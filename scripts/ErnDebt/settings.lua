d1ef3143be9e61054929bfae93891bb36902f74a
Erin Pentecost <erin@pentecost.email>
2026-01-25 13:53:58 -0800

    init

---
+++ scripts/ErnDebt/settings.lua
@@ -0,0 +1,94 @@
+--[[
+ErnDebt for OpenMW.
+Copyright (C) 2026 Erin Pentecost
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as
+published by the Free Software Foundation, either version 3 of the
+License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+]]
+local interfaces   = require("openmw.interfaces")
+local storage      = require("openmw.storage")
+local MOD_NAME     = require("scripts.ErnDebt.ns")
+local util         = require('openmw.util')
+
+local mainGroupKey = "Settings/" .. MOD_NAME
+
+local function init()
+    interfaces.Settings.registerPage {
+        key = MOD_NAME,
+        l10n = MOD_NAME,
+        name = "name",
+        description = "description",
+    }
+
+    interfaces.Settings.registerGroup {
+        key = mainGroupKey,
+        page = MOD_NAME,
+        l10n = MOD_NAME,
+        name = "settings",
+        permanentStorage = true,
+        settings = {
+            {
+                key = "interest",
+                name = "interestName",
+                description = "interestDescription",
+                default = 0.05,
+                renderer = "number",
+                argument = {
+                    integer = false,
+                    min = 0,
+                    max = 1000,
+                }
+            },
+        }
+    }
+end
+
+local lookupFuncTable = {
+    __index = function(table, key)
+        if key == "subscribe" then
+            return function(callback)
+                print("Subscribed to " .. tostring(table.groupKey) .. ".")
+                return table.section.subscribe(table.section, callback)
+            end
+        elseif key == "section" then
+            return table.section
+        elseif key == "groupKey" then
+            return table.groupKey
+        end
+        -- fall through to settings section
+        local val = table.section:get(key)
+        if val ~= nil then
+            return val
+        else
+            error("unknown setting " .. tostring(key))
+        end
+    end,
+}
+
+local mainContainer = {
+    groupKey = mainGroupKey,
+    section = storage.playerSection(mainGroupKey)
+}
+setmetatable(mainContainer, lookupFuncTable)
+
+---@alias SettingContainer table
+
+---@class Settings
+---@field init fun()
+---@field main SettingContainer
+
+---@type Settings
+return {
+    init = init,
+    main = mainContainer,
+}
