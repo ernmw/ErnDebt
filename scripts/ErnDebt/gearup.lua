acac43a92e2e20d80855df6113318f49865dc3a5
Erin Pentecost <erin@pentecost.email>
2026-01-26 18:43:31 -0800

    more gear packages

--- scripts/ErnDebt/gearup.lua
+++ scripts/ErnDebt/gearup.lua
@@ -30,20 +30,16 @@ local commonPants  = { "common_pants_01", "common_pants_02", "common_pants_03", 
     "T_Com_Cm_Pants_01",
     "T_Com_Cm_Pants_02",
     "T_Com_Cm_Pants_03",
-    "T_Com_Cm_Pants_04",
-    "T_Imp_Cm_PantsColNorth_01",
-    "T_Imp_Cm_PantsColNorth_02",
-    "T_Imp_Cm_PantsColNorth_03",
-    "T_Imp_Cm_PantsColWest_06",
-    "T_Imp_Cm_PantsNavy_01",
-    "T_Rga_Cm_Pants_01" }
+    "T_Com_Cm_Pants_04" }
 local commonShirts = { "common_shirt_01", "common_shirt_02", "common_shirt_03", "common_shirt_04", "common_shirt_05",
     "T_Com_Cm_Shirt_03", "T_Com_Cm_Shirt_04" }
 
 
 -- a random package is chosen, provided it meets the requirements.
+-- at least one element in each primitive list must be in the base game.
 local gearPackages = {
     {
+        name = "1h grunt",
         level = 1,
         equipment = {
             [SLOT.Helmet] = { "bonemold_helm", "T_De_Bonemold_HelmOpen_01" },
@@ -65,13 +61,14 @@ local gearPackages = {
                 "T_Imp_Cm_BootsCol_03",
                 "T_Imp_Cm_BootsCol_04" },
         },
-        extra = { "p_recall_s", "Potion_Local_Brew_01" },
+        extra = { { "Potion_Local_Brew_01" }, { "p_restore_health_b" } },
     },
     {
-        level = 3,
+        name = "2h grunt",
+        level = 1,
         equipment = {
-            [SLOT.LeftPauldron] = { "T_De_Chitin_PauldrL_01" },
-            [SLOT.RightPauldron] = { "T_De_Chitin_PauldrR_01" },
+            [SLOT.LeftPauldron] = { "T_De_Chitin_PauldrL_01", "chitin pauldron - left" },
+            [SLOT.RightPauldron] = { "T_De_Chitin_PauldrR_01", "chitin pauldron - right" },
             [SLOT.Helmet] = { "chitin_mask_helm", "T_Com_Iron_Helm_01" },
             [SLOT.Cuirass] = { "nordic_ringmail_cuirass" },
             [SLOT.Greaves] = { "netch_leather_greaves", "T_Imp_StuddedLeather_Greaves_01" },
@@ -82,7 +79,25 @@ local gearPackages = {
             [SLOT.RightGauntlet] = { "iron_gauntlet_right" },
             [SLOT.Boots] = { "chitin boots", "T_De_Guarskin_Boots_01" },
         },
-        extra = { "p_fortify_fatigue_s", "p_fortify_health_s" },
+        extra = { { "p_fortify_fatigue_s" }, { "p_fortify_health_s" }, { "potion_comberry_wine_01" } },
+    },
+    {
+        name = "ranged grunt",
+        level = 1,
+        equipment = {
+            [SLOT.Helmet] = { "netch_leather_boiled_helm", "T_Imp_Cm_HatColWest_01" },
+            [SLOT.Cuirass] = { "netch_leather_cuirass", "T_De_NetchRogue_Cuirass_01", },
+            [SLOT.CarriedRight] = { "chitin short bow" },
+            [SLOT.Ammunition] = { "chitin arrow" },
+            [SLOT.Shirt] = commonShirts,
+            [SLOT.Pants] = commonPants,
+            [SLOT.LeftGauntlet] = { "common_glove_left_01" },
+            [SLOT.RightGauntlet] = { "T_Nor_Leather1_BarcerR_01", "right leather bracer", "cloth bracer left" },
+            [SLOT.Boots] = {
+                "netch_leather_boots",
+                "chitin boots" },
+        },
+        extra = { { "p_magicka_resistance_b" }, { "p_lightning shield_c" } },
     },
 }
 
@@ -120,7 +135,8 @@ local function gearupNPC(npc, pcLevel)
     for slot, itemList in pairs(gearTable.equipment) do
         local itemRecordId = selectRecordFromList(itemList)
         if itemRecordId then
-            local equipmentObject = world.createObject(itemRecordId)
+            local count = slot == SLOT.Ammunition and math.random(10, 20) or 1
+            local equipmentObject = world.createObject(itemRecordId, count)
             equipmentObject:moveInto(inventory)
             toEquip[slot] = equipmentObject
         else
@@ -131,7 +147,8 @@ local function gearupNPC(npc, pcLevel)
     npc:sendEvent(MOD_NAME .. "onEquip", toEquip)
 
     -- insert some extra stuff
-    local itemRecordId = selectRecordFromList(gearTable.extra)
+    local extraList = gearTable.extra[math.random(1, #gearTable.extra)]
+    local itemRecordId = selectRecordFromList(extraList)
     if itemRecordId then
         local equipmentObject = world.createObject(itemRecordId)
         equipmentObject:moveInto(inventory)
